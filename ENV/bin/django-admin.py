bc70afc6cfa698e5ef7ec4f8c995e019b440fcec
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-10-23 16:00:28 +0200

    initialised requirements.txt ENV and Django

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/raphaelbrunner/Documents/Projects/ProjectBirth/ENV/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
